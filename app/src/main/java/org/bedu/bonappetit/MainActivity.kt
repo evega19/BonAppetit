76aedd4a7188b9f80f77211c2d3e9fa0ba21c451
Janner <janner.work@gmail.com>
2021-10-04 17:57:51 -0500

    Agregando el Inicio. Burdo para despues ir arreglandolo

--- app/src/main/java/org/bedu/bonappetit/MainActivity.kt
+++ app/src/main/java/org/bedu/bonappetit/MainActivity.kt
@@ -1,17 +1,39 @@
 package org.bedu.bonappetit
 
+import android.animation.AnimatorInflater
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.android.gms.tasks.OnCompleteListener
+import org.bedu.bonappetit.databinding.ActivityMainBinding
 
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, Act1_1BeforeLogin::class.java))
+            finish()
+        }, 2800)
+
+        screen()
+
+
+
+
+
+
 
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
             if (!task.isSuccessful) {
@@ -22,14 +44,44 @@ class MainActivity : AppCompatActivity() {
             val token = task.result
 
             Log.d("FCM_TOKEN",token!!)
-            Toast.makeText(baseContext,"FCM token: $token", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(baseContext,"FCM token: $token", Toast.LENGTH_SHORT).show()
         })
 
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
         /*val analitycs : FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
         val bundle = Bundle()
         bundle.putString("message", "integración de firebase completa")
         analitycs.logEvent("InitScreen",bundle)*/
+
+
+
     }
+
+
+    private fun screen() {
+
+        /*ObjectAnimator.ofFloat(arwing, "translationX", 200f).apply {
+            duration = 3000
+            interpolator = CycleInterpolator(1f)
+            start()
+        }*/
+
+        AnimatorInflater.loadAnimator(this, R.animator.screen).apply {
+            setTarget(binding.LogoScreen)
+            start()
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
