29eacb883fd2e6a3733b50559dc1dd9e2a4d3f39
Erick Vega <erick190796@hotmail.com>
2021-10-04 01:33:33 -0500

    Modificación menor para que al momento que se pague regrese al before login

--- app/src/main/java/org/bedu/bonappetit/Act4Payment.kt
+++ app/src/main/java/org/bedu/bonappetit/Act4Payment.kt
@@ -64,7 +64,7 @@ class Act4Payment : AppCompatActivity() {
 
     private fun goToMain(){
         sharedPreferences?.edit()?.clear()?.apply()
-        val i = Intent(this, Act2ScannerTableCode::class.java)
+        val i = Intent(this, Act1_1BeforeLogin::class.java)
         i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(i)
     }
