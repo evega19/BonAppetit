de886d0127ba2dc864a93ff93077a61f76118baa
Dyeseniact <camposdyes@gmail.com>
2021-10-04 09:32:07 -0500

    Agregando notificaciones de bienvenida y de ofertas.

--- app/src/main/java/org/bedu/bonappetit/Act1_2Login.kt
+++ app/src/main/java/org/bedu/bonappetit/Act1_2Login.kt
@@ -1,11 +1,74 @@
 package org.bedu.bonappetit
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 
 class Act1_2Login : AppCompatActivity() {
+
+    companion object {
+        const val CHANNEL_COURSES = "CHANNEL_COURSES"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_act12_login)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setNotificationChannel()
+        }
+
+
+        /* Poner esta función cuando haga click el ingresar para que le llegue la notificacion de ofertas*/
+        offersNotification()
+
     }
-}
+
+
+/*-----------Código que envia la notificacion con las ofertas------------*/
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setNotificationChannel(){
+        val name = getString(R.string.channel_courses) //Nombre del canal
+        val descriptionText = getString(R.string.courses_description) //descriptcion de channel
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(Act1_3Register.CHANNEL_COURSES,name,importance).apply {
+            description =descriptionText
+        }
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(channel)
+
+    }
+
+    private fun offersNotification(){
+        val intent = Intent(this, Act1_4Offers::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val notification = NotificationCompat.Builder(this, Act1_3Register.CHANNEL_COURSES)
+            .setSmallIcon(R.drawable.iconopizza)
+            .setColor(ContextCompat.getColor(this,R.color.colorPrimaryVariant))
+            .setContentTitle(getString(R.string.action_title))
+            .setContentText(getString(R.string.action_body))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        NotificationManagerCompat.from(this).run {
+            notify(20, notification)
+        }
+
+    }
--- app/src/main/java/org/bedu/bonappetit/Act1_3Register.kt
+++ app/src/main/java/org/bedu/bonappetit/Act1_3Register.kt
@@ -1,15 +1,21 @@
 package org.bedu.bonappetit
 
+
 import android.app.AlertDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-
-
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 
 
 class Act1_3Register : AppCompatActivity() {
@@ -17,9 +23,19 @@ class Act1_3Register : AppCompatActivity() {
     private lateinit var sharedPreferences : SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
 
+
+    companion object {
+        const val CHANNEL_COURSES = "CHANNEL_COURSES"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_act13_register)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setNotificationChannel()
+        }
+
 
         val name = findViewById<TextView>(R.id.editText_name)
         val userName = findViewById<TextView>(R.id.editText_userName)
@@ -34,7 +50,10 @@ class Act1_3Register : AppCompatActivity() {
         buttonRegistrar.setOnClickListener {
             if((name.text.trim().isNotEmpty()) && (userName.text.trim().isNotEmpty()) && (email.text.trim().isNotEmpty()) && (password.text.trim().isNotEmpty()) && (phone.text.trim().isNotEmpty()) && checkTerms.isChecked){
 
-                mensaje("¡Bienvenido a la comunidad!","Bon Appetit")
+                startActivity(Intent(applicationContext, Act2ScannerTableCode::class.java))
+                simpleNotification()
+                //offersNotification()
+
             }else{
                 Toast.makeText(this,"Error, debes de llenar todos los campos y aceptar los terminos y condiciones.", Toast.LENGTH_LONG).show()
             }
@@ -65,18 +84,58 @@ class Act1_3Register : AppCompatActivity() {
 
 
     }
-    private fun mensaje(title:String,message:String){
-        AlertDialog.Builder(this)
-            .setTitle(title)
-            .setMessage(message)
-            .setPositiveButton("OK"){dialogInterface, which ->
-                val intent = Intent(this,Act3Frag1AllMenu::class.java)
-                startActivity(intent)
-                //loginactive?.finish()
-                finish()
-            }
-            .create()
-            .show()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setNotificationChannel(){
+        val name = getString(R.string.channel_courses) //Nombre del canal
+        val descriptionText = getString(R.string.courses_description) //descriptcion de channel
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_COURSES,name,importance).apply {
+            description =descriptionText
+        }
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(channel)
+
+    }
+
+    private fun simpleNotification(){
+        val notification = NotificationCompat.Builder(this, CHANNEL_COURSES )
+            .setSmallIcon(R.drawable.iconopizza)
+            .setColor(ContextCompat.getColor(this,R.color.colorPrimaryVariant))
+            .setContentTitle(getString(R.string.simple_title))
+            .setContentText(getString(R.string.simple_body))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        NotificationManagerCompat.from(this).run {
+            notify(20,notification)
+        }
+    }
+
+    private fun offersNotification(){
+        val intent = Intent(this, Act1_4Offers::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_COURSES )
+            .setSmallIcon(R.drawable.iconopizza)
+            .setColor(ContextCompat.getColor(this,R.color.colorPrimaryVariant))
+            .setContentTitle(getString(R.string.action_title))
+            .setContentText(getString(R.string.action_body))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        NotificationManagerCompat.from(this).run {
+            notify(20, notification)
+        }
+
+
     }
 
 
