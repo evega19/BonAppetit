28f26e1887862a732291257c59ecb62ae40fec04
genarokepkana <genaromarcosac@outlook.com>
2021-10-18 11:58:55 -0500

    Se agrega autentificacion con google

--- app/src/main/java/org/bedu/bonappetit/Act1_2Login.kt
+++ app/src/main/java/org/bedu/bonappetit/Act1_2Login.kt
@@ -3,23 +3,55 @@ package org.bedu.bonappetit
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import org.bedu.bonappetit.databinding.ActivityAct12LoginBinding
+import org.bedu.bonappetit.databinding.ActivityAct13RegisterBinding
+import org.bedu.bonappetit.util.Utility
+import org.bedu.bonappetit.util.Utility.hideKeyboard
 
+enum class ProviderType{
+    BASIC,
+    GOOGLE
+}
 class Act1_2Login : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var buttonLogin : Button
+    private lateinit var email : TextView
+    private lateinit var password : TextView
+    private lateinit var binding: ActivityAct12LoginBinding
+    var Ale:Int=1
+    private var RC_SIGN_IN : Int  = 123
+
+
+
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     companion object {
         const val CHANNEL_COURSES = "CHANNEL_COURSES"
@@ -27,18 +59,116 @@ class Act1_2Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_act12_login)
+        binding = ActivityAct12LoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         auth = Firebase.auth
 
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             setNotificationChannel()
         }
+        email = binding.editTextEmail
+        password = binding.editTextPassword
 
-
+        handleClick()
         /* Poner esta función cuando haga click el ingresar para que le llegue la notificacion de ofertas*/
         offersNotification()
+
+        FirebaseApp.initializeApp(this)
+/*
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)*/
+
+        auth = Firebase.auth
     }
+
+
+    private fun handleClick() {
+
+        binding.btnIngresar.setOnClickListener {
+            it.hideKeyboard()
+
+            binding.btnIngresar.visibility = View.GONE
+
+            val email = email.text.trim().toString()
+            val password = password.text.trim().toString()
+
+            signIn(email, password)
+        }
+        /*binding.btnGoogle.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }*/
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                Utility.displaySnackBar(binding.root, "Google sign in failed", this, R.color.red)
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user, null)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null, task.exception)
+                }
+            }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:Succes")
+                    val user = auth.currentUser
+                    updateUI(user, null)
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    updateUI(null, task.exception)
+                }
+
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?, exception: Exception?) {
+        binding.btnGoogle.visibility = View.VISIBLE
+        if (exception != null) {
+            binding.btnGoogle.visibility = View.VISIBLE
+            Utility.displaySnackBar(binding.root, exception.message.toString(), this, R.color.red)
+        } else {
+            Utility.displaySnackBar(binding.root, "Login was successful: ", this, R.color.green)
+            binding.btnGoogle.visibility = View.VISIBLE
+        }
+    }
+
+
+
     /*-----------Código que envia la notificacion con las ofertas------------*/
     @RequiresApi(Build.VERSION_CODES.O)
     private fun setNotificationChannel() {
